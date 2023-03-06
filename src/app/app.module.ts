c3719d8c5384e545c4c08ab42bc848fc20b280bc
Jhedie <jeddiahawuku12@gmail.com>
2023-03-06 17:38:51 +0000

    feat: Create simple html structure
    Add default style for app component
    
    Create current events component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CurrentEventsComponent } from './current-events/current-events.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CurrentEventsComponent
   ],
   imports: [
     BrowserModule
