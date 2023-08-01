742f63e1daa305094652937c45e73467447bb944
Jhedie <jeddiahawuku12@gmail.com>
2023-08-01 21:22:56 +0100

    feat: Implement rough get request for all events

--- server/localeventapp/routes/events.js
+++ server/localeventapp/routes/events.js
@@ -1,9 +1,36 @@
 import express from "express";
+import axios from "axios";
+const config = require("./config.json");
 
 const router = express.Router();
-//all routes in here are starting with /events
-router.get("/", (req, res) => {
-  res.send(["event1", "event2"]);
+const ROOT_URL = config.rootUrl;
+const API_KEY = config.apiKey;
+const ENCODED_API_KEY = encodeURIComponent(API_KEY);
+
+/**
+ * all routes in here are starting with /events
+
+ * Get all events
+ */
+router.get("/", async (req, res) => {
+  try {
+    const response = await axios(
+      `${ROOT_URL}/events.json?apikey=${ENCODED_API_KEY}`
+    );
+    console.log("response", response.data);
+    res.send(response.data._embedded.events);
+  } catch (error) {
+    console.error(error);
+  }
 });
 
+/**
+ * Get a specific event by id
+ */
+// router.get("/:id", async (req, res) => {
+//   try {
+//     const response = await axios()
+//   }
+
+// });
 export default router;
