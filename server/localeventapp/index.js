6d1312e40fcecfcf8658bf7c22ae1bf7e5fa6187
Jhedie <jeddiahawuku12@gmail.com>
2023-08-01 22:24:29 +0100

    feat: Implement CORS on server

--- server/localeventapp/index.js
+++ server/localeventapp/index.js
@@ -1,7 +1,9 @@
 import express from "express";
 const app = express();
 import bodyParser from "body-parser";
-
+import cors from "cors";
+const corsOptions = { origin: "http://localhost:3000" };
+app.use(cors(corsOptions));
 //using body parser to parse json bodies into JS objects
 app.use(bodyParser.json());
 
